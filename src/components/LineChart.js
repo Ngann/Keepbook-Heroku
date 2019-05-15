2f5da40605274304ac4939b1985e65cd89912588
Ngan Nguyen <ngann.dev@gmail.com>
2019-05-15 14:55:12 -0700

    update chart color

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -19,11 +19,11 @@ const data = {
       pointBorderWidth: 1,
       pointHoverRadius: 5,
       pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderColor: 'rgba(0,128,0,10)',
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
+      data: [65, 59, 80, 81, 90, 95, 100]
     }
   ]
 };
