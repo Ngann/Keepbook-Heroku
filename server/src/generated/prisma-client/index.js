503872504eeabfac57dd9faf9f71d4992692fa45
Ngan Nguyen <ngann.dev@gmail.com>
2019-05-17 14:20:29 -0700

    save point

--- server/src/generated/prisma-client/index.js
+++ server/src/generated/prisma-client/index.js
@@ -28,6 +28,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://eu1.prisma.sh/ngan-nguyen-13344c/server/dev`
+  endpoint: `https://eu1.prisma.sh/ngan-nguyen-13344c/vendor/dev`
 });
 exports.prisma = new exports.Prisma();
