d0c1d84aff9fee6955ea80fef19c8d473d85f997
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-05-21 16:14:30 -0700

    add table to search bar and center search

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
-
+import Table from 'react-bootstrap/Table'
 
 class Bill extends Component {
   render() {
     return (
       <div>
-        <div>
-          {this.props.bill.vendor} {this.props.bill.date} {this.props.bill.account} {this.props.bill.amount} 
-        </div>
+         <Table striped hover size="sm" >
+          <thead >
+          </thead>
+          <tbody>
+            <tr style={{display: 'flex'}}>
+              <td>{this.props.bill.date}</td>
+              <td>{this.props.bill.vendor}</td>
+              <td>{this.props.bill.account}</td>
+              <td>{this.props.bill.amount}</td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
     )
   }
